91d0c6808ee6f726048cc2036945005d9ed172c7
dianlujitao <dianlujitao@lineageos.org>
2019-02-14 20:53:36 +0800

    sepolicy: qcom: Rename common to vendor to avoid confusion
     * Remove platform specific rule from sepolicy.mk as well, it's never
       ever a valid usecase.
    
    Change-Id: I8bf6af30407d55beb9b2c5a2afaca091564e1605
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -14,6 +14,5 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
 
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
-    device/pixeldust/sepolicy/qcom/common \
-    device/pixeldust/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+    device/pixeldust/sepolicy/qcom/vendor
 endif
