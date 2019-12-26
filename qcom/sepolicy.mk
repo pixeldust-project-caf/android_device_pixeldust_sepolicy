eb0694eabd790b36c9c3184eb9e38d914c46c07d
Dyneteve <dyneteve@pixelexperience.org>
2019-12-26 17:38:21 +0200

    sepolicy: Finish cleaning up
    Change-Id: Ic1c17abcb6cfb36934ee61f139f980b5a2b7bb70
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -9,10 +9,7 @@ TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
 endif
 endif
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/private
-
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/vendor
+    device/custom/sepolicy/qcom/vendor
 endif
