pumpac7i0n/android_device_pa_sepolicy

sepolicy: add system-only directory to common and qcom
Change-Id: Ic32d6e86e2796225b894ca5a56280773036c2cbf

common/sepolicy.mk
@@ -17,7 +17,8 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/pa/sepolicy/common/dynamic
+    device/pa/sepolicy/common/dynamic \
+    device/pa/sepolicy/common/system-only
 else
 BOARD_SEPOLICY_DIRS += \
     device/pa/sepolicy/common/dynamic \

qcom/sepolicy.mk
@@ -14,7 +14,8 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/pa/sepolicy/qcom/dynamic
+    device/pa/sepolicy/qcom/dynamic \
+    device/pa/sepolicy/qcom/system-only
 else
 BOARD_SEPOLICY_DIRS += \
     device/pa/sepolicy/qcom/dynamic \

pixeldust-project-caf/android_device_pixeldust_sepolicy

Signed-off-by: spezi77 <spezi7713@gmx.net>

common/sepolicy.mk
@@ -17,7 +17,8 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/pixeldust/sepolicy/common/dynamic
+    device/pixeldust/sepolicy/common/dynamic \
+    device/pixeldust/sepolicy/common/system-only
 else
 BOARD_SEPOLICY_DIRS += \
     device/pixeldust/sepolicy/common/dynamic \

qcom/sepolicy.mk
@@ -10,6 +10,10 @@ endif
 endif
 
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/pixeldust/sepolicy/qcom/dynamic \
+    device/pixeldust/sepolicy/qcom/system-only
+else
 BOARD_SEPOLICY_DIRS += \
     device/pixeldust/sepolicy/qcom/vendor
 endif